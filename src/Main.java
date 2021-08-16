53e38924aebc69cea29f8d525df02d84e8520708
Maria Vostrikova <vostrikova.maria2017@yandex.ru>
2021-08-17 00:22:44 +0300

    Main.class added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 15000;
+        int amount = 20;
+        int bonusMile = cost / amount;
+        System.out.println(bonusMile);
+    }
+}
